31c8495e03d42a9ae510e3996964161246b3e985
Jon Kurtzweil <jonk@jon.local>
2020-04-13 01:52:43 -0500

    Random roll support added back in

--- app/utils/classes.py
+++ app/utils/classes.py
@@ -1,6 +1,8 @@
 import unittest
 import pathlib
 import json
+import random
+from collections import defaultdict
 
 BASE_DIRECTORY = pathlib.Path.cwd()
 TABLES_DIRECTORY = BASE_DIRECTORY / 'app' / 'utils' / 'json_tables'
@@ -31,23 +33,16 @@ class Trap(BaseTable):
 
 class EncounterRegion:
     def __init__(self, region_name: str):
-        #print(region)
-        #base_table = BaseTable(region)
-        #for region in base_table.tables:
-        #    print(region)
-        self.biomes = {}
-        self.terrain = {}
+        self.biomes = defaultdict(Biome)
         base_table = BaseTable('regions').tables
         for region in base_table:
-            for biome in base_table[region]['biomes']:
-                new_biome = Biome(biome)
-                self.terrain.update(new_biome.terrain)
-        #print(base_table)
-        #for region in base_table.tables:
-            #biome_list = region['biomes']
-            #for biome in region['biomes']:
-            #print(biome_list)
-            #pass
+            for biome_key in base_table[region]['biomes']:
+                new_biome = Biome(biome_key)
+                self.biomes[biome_key] = new_biome
+
+    def rollRegion(self):
+        biome_key = random.choice(list(self.biomes))
+        return self.biomes[biome_key]
 
 class Biome:
     def __init__(self, target_biome: str):
@@ -59,6 +54,38 @@ class Biome:
         self.climate = base_table[target_biome]['climate']
         self.loot_tables = base_table[target_biome]['loot_tables']
         self.foraging = base_table[target_biome]['terrain']
+
+    def rollTerrain(self):
+        return random.choice(list(self.terrain))
+
+    def rollObstacles(self):
+        return random.choice(list(self.obstacles))
+
+    def rollBestiary(self):
+        #TODO: Flesh out bestiary to include full stat block plus ultra
+        #TODO; Maybe include enemy nummbers and common groupings and roll on tables from there
+        #TODO; Add in What are the Monsters Doing roll
+        #return random.choice(list(self.bestiary))
+        return "Placeholder"
+
+    def rollClimate(self):
+        #TODO: Create base weather tables
+        #TODO: Factor in humidity and temperature somehow
+        #TODO: Allow for overrides from special weather
+        #TODO: Return the result here
+        return None#random.choice()
+
+    def rollLoot(self):
+        #TODO: Read in actual loot tables using the keys this class has at this point
+        #TODO: Flesh out the structure of a random loot table to allow rollowing on that table
+        #TODO: Return the result here
+        return None#random.choice(self.loot_tables)
+
+    def rollForaging(self):
+        #TODO: Roll on each of the foraging tables and make a dict/list
+        #TODO: Return the whole shebang here probably
+        # Doesn't seem likely DM will do this often enough to need us to make them pick, they can roll 1d4 or whatever?
+        return None#random.choice(list(self.foraging))
 
 class ValidateTables:
     def __init__(self):
@@ -72,7 +99,7 @@ class ValidateTables:
 class ClassesTest(unittest.TestCase):
     def test_base_table(self):
         base_table = BaseTable('biomes')
-        #self.assertIsInstance(base_table.tables['city'], dict)
+        self.assertIsInstance(base_table.tables['city'], dict)
 
     def test_base_table_fails_init_if_no_output(self):
         with self.assertRaises(ValueError):
@@ -98,7 +125,7 @@ class ClassesTest(unittest.TestCase):
         self.assertEqual(base_trap.fumble_injury, 'Puncture Wound')
         self.assertEqual(base_trap.fumble_penalty, '-1 Con')
 
-    def test_biomes_class_has_expected_attribrutes(self):
+    def test_biomes_class_has_expected_attributes(self):
         base_biome = Biome('salt_marsh')
         self.assertIsInstance(base_biome.display_name, str)
         self.assertIsInstance(base_biome.terrain, dict)
@@ -108,11 +135,23 @@ class ClassesTest(unittest.TestCase):
         self.assertIsInstance(base_biome.loot_tables, list)
         self.assertIsInstance(base_biome.foraging, dict)
 
+    def test_biomes_class_rolls_attributes(self):
+        base_biome = Biome('salt_marsh')
+        self.assertIsInstance(base_biome.rollTerrain(), str)
+        self.assertIsInstance(base_biome.rollObstacles(), str)
+        self.assertIsInstance(base_biome.rollBestiary(), str)
+        self.assertIsNone(base_biome.rollClimate())
+        self.assertIsNone(base_biome.rollLoot())
+        self.assertIsNone(base_biome.rollForaging())
 
-
-    def test_region_class_has_expected_attributes(self):
+    def test_encounter_region_class_has_expected_attributes(self):
         test_region = EncounterRegion('port_vyshaan')
-        #self.assertIsInstance(test_region.biomes, dict)
+        self.assertIsInstance(test_region.biomes, dict)
+
+    def test_encounter_region_class_returns_random_biomes(self):
+        test_region = EncounterRegion('port_vyshaan')
+        random_biome = test_region.rollRegion()
+        self.assertIsInstance(random_biome, Biome)
 
 if __name__ == '__main__':
     unittest.main()
