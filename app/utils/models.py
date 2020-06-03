db7a5df9aa8894057a6fd6a80fea03f360aa0719
j-kurtzweil <jon.b.kurtzweil@gmail.com>
2020-06-03 16:33:12 -0500

    f

--- app/utils/models.py
+++ app/utils/models.py
@@ -1,14 +1,14 @@
-from flask_sqlalchemy import SQLAlchemy
-from app import create_app, db
-
-db.Model.metadata.reflect(db.engine)
-
-class Monster(db.Model):
-    __tablename__ = 'monsters'
-    __table_args__ = { 'extend_existing': True }
-    name = db.Column(db.Text, primary_key=True)
-
-class BiomeMonster(db.Model):
-    __tablename__ = 'biome_monsters'
-    __table_args__ = { 'extend_existing': True }
-    monster_id = db.Column(db.Integer, primary_key=True)
+#from flask_sqlalchemy import SQLAlchemy
+##from app import create_app, db
+#
+#db.Model.metadata.reflect(db.engine)
+#
+#class Monster(db.Model):
+#    __tablename__ = 'monsters'
+#    __table_args__ = { 'extend_existing': True }
+#    name = db.Column(db.Text, primary_key=True)#
+#
+#class BiomeMonster(db.Model):
+#    __tablename__ = 'biome_monsters'
+#    __table_args__ = { 'extend_existing': True }
+#    monster_id = db.Column(db.Integer, primary_key=True)
